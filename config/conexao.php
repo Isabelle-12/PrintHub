9a29c09d05a2af9d6815135c8f1028580d4c115c
Isabelle Faculdade <isabelleribeirodossantos12@gmail.com>
2026-03-31 15:30:46 -0300

    implementação de nova conexao com banco de dados e novo banco

--- config/conexao.php
+++ config/conexao.php
@@ -1,18 +1,21 @@
 <?php
 
-$servidor   = "localhost:3306";
-$usuario    = "root";
-$senha      = "q81SL27@";
+$servidor   = "localhost";
+$usuario    = "printly_user";
+$senha      = "123456";
 $nome_banco = "printly_db";
 
 $conexao = new mysqli($servidor, $usuario, $senha, $nome_banco);
 
 if ($conexao->connect_error) {
-    header("Content-type: application/json;charset:utf-8");
+    header("Content-type: application/json;charset=utf-8");
     echo json_encode([
         "status"   => "nok",
-        "mensagem" => "Erro de conexão: " . $conexao->connect_error,
+        "mensagem" => "Erro ao conectar ao banco de dados",
         "data"     => []
     ]);
     exit;
 }
+
+
+$conexao->set_charset("utf8mb4");
