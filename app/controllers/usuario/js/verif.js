6a0d3330b497890b368b24d87d4fa8692be5a1e3
Paula Mendes Pontes <paulacmp07@gmail.com>
2026-04-09 15:01:16 -0300

    permitindo a visualização de abas da navbar

--- app/controllers/usuario/js/verif.js
+++ app/controllers/usuario/js/verif.js
@@ -1,44 +1,49 @@
-async function verif() {
-    const resp = await fetch("../config/verificar.php");
-    const dados = await resp.json();
+    async function verif() {
+        const resp = await fetch("../config/verificar.php");
+        const dados = await resp.json();
 
-    if (!dados.logado) {
-        alert("Você precisa estar logado!");
-        window.location.href = "index.php?rota=login";
+        if (!dados.logado) {
+            alert("Você precisa estar logado!");
+            window.location.href = "index.php?rota=login";
+        }
+
+       if (dados.tipos === "CLIENTE") {
+
+        document.getElementById("notif")?.classList.add("d-none");
+        document.getElementById("usuario")?.classList.add("d-none");
     }
 
+        const acesso = document.body.getAttribute("data-acesso");
+        
+        if (acesso && !acesso.split(",").includes(dados.tipos)) {
+            alert("Você não tem permissão para acessar esta página.");
+            window.location.href = "index.php?rota=home";
+            return;
+        }
+        
 
-    const acesso = document.body.getAttribute("data-acesso");
-    
-    if (acesso && !acesso.split(",").includes(dados.tipos)) {
-        alert("Você não tem permissão para acessar esta página.");
-        window.location.href = "index.php?rota=home";
-        return;
+        
+        console.log("Usuário logado:", dados.email);
     }
-    
 
-    
-    console.log("Usuário logado:", dados.email);
-}
+    async function sair() {
+    await fetch("../config/logout.php");
 
-  async function sair() {
-  await fetch("../config/logout.php");
+    alert('Saindo...');
 
-  alert('Saindo...');
-
-  window.location.href = "index.php?rota=home";
+    window.location.href = "index.php?rota=home";
 
 
-}
-
-// nao sei se deixo no dom ja que nao está no header, mas deixarei
-window.addEventListener('DOMContentLoaded', () => {
-    verif();
-    const btnSair = document.getElementById("sair");
-    if (btnSair) {
-        btnSair.addEventListener("click", sair);
     }
-});
+
+    // nao sei se deixo no dom ja que nao está no header, mas deixarei
+    window.addEventListener('DOMContentLoaded', () => {
+        verif();
+        const btnSair = document.getElementById("sair");
+        if (btnSair) {
+            btnSair.addEventListener("click", sair);
+        }
+    });
 
 
 
